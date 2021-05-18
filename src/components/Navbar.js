a2788b6d53443945e246efd9d9b0f476ef715fb1
Krishanathep Jareanpong <krishanathep@gmail.com>
2021-05-18 16:27:48 +0700

    include navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,38 @@
 import { Navbar, Nav } from "react-bootstrap";
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" expand="lg" variant="dark">
       <div className="container">
-        <NavLink className='navbar-brand' to='/'>
-          <i class="fab fa-github fa-2x"></i> Hello World
+        <NavLink className="navbar-brand" to="/">
+          <i class="fab fa-github fa-lg"></i> Navbar scroll
         </NavLink>
-        <Nav className="ml-auto">
-            <NavLink to='/' className='nav-link' exact activeClassName='active'>Home</NavLink>
-            <NavLink to='/about' className='nav-link' activeClassName='active'>About</NavLink> 
-            <NavLink to='/courses' className='nav-link' activeClassName='active'>Courses</NavLink>
-            <NavLink to='/products' className='nav-link' activeClassName='active'>Products</NavLink>
-        </Nav>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="ml-auto">
+            <NavLink to="/" className="nav-link" exact activeClassName="active">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="nav-link" activeClassName="active">
+              About
+            </NavLink>
+            <NavLink
+              to="/courses"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Courses
+            </NavLink>
+            <NavLink
+              to="/products"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Products
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
       </div>
     </Navbar>
   );
