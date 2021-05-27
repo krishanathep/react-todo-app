4ef75996a1787fcb29f076a77913dcaca93f7518
Krishanathep <krishanathep@MacBook-Air.local>
2021-05-27 21:07:52 +0700

    Include Bootstrap

---
+++ src/components/ConTent.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+
+const ConTent = () => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className='my-5'>
+            <h3 className="text-center">PROJECT</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="4" className="my-2">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="4" className="my-2">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="4" className="my-2">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default ConTent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,13 @@
-import React, {useState} from "react";
-import { Modal, Button } from 'react-bootstrap'
+import React from "react";
+import JumboTron from '../components/JumboTron'
+import ConTent from '../components/ConTent'
 
 const Home = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <div className='container'>
-      <Button variant="primary mt-3" onClick={handleShow}>
-        Launch static backdrop modal
-      </Button>
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
-      </Modal>
+      <JumboTron/>
+      <ConTent/>
     </div>
   );
 };
